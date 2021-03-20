1b0b36432caf647a4d67a11f3259c74caae2bd9f
Hanzo-R <78445031+Hanzo-R@users.noreply.github.com>
2021-03-19 23:04:08 -0700

    Done Lab 05-01

--- part-1/mind_reader.cc
+++ part-1/mind_reader.cc
@@ -1,143 +1,211 @@
+// Hans Gabrielle Repotente
+// CPSC 120-01
+// 2021-03-11
+// hanssr@csu.fullerton.edu
+// @Hanzo-R
+//
+// Lab 05-01
+//
+// A program that reads your mind.
+//
 
-// TODO: Add a #include for the iostream, cstdlib, and string header files
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <array>
 
 using namespace std;
 
-// TODO: Write a function that prints out an error message; the function returns
+//   Write a function that prints out an error message; the function returns
 // void and has no parameters
+void ErrorMessage() {
+  cout << "This program may only be used by a qualified technician.\n";
+}
 
-/// Prints out an error message. Takes no arguments and returns nothing.
-void ErrorMessage() {}
-
-// TODO: Write a function named CardValue; the function returns an int and takes
+//   Write a function named CardValue; the function returns an int and takes
 // a const string& as a parameter
+int CardValue(const string& string_value) {
+  string card_value = string(1, string_value.at(0));
+  int card_v= 0;
+  if (card_value == "A") {
+    card_value = "1";
+  }else if (card_value == "J"){
+    card_value = "11";
+  }else if (card_value == "Q"){
+    card_value = "12";
+  }else if (card_value == "K"){
+    card_value = "13";
+  }
+  else{
+    card_value = string(1, string_value.at(0));
+  }
+  try{
+    if (card_value == "A" || "1" || "J" || "Q" || "K"){
+      card_v = stoi(card_value);
+    } else{
+      card_v = stoi(card_value) - 1;
+    }
+  }catch (const exception& e){
+    ErrorMessage();
+    return 1;
+  }
 
-/// SimpleValue takes the \p face_value of a card as a string
-/// and returns the simple_value of the card as an int
-///
-/// \param face_value A string representing the value of a card
-/// (A, 2, ..., Q, K)
-///
-/// \returns The simple_value of the card as an int (0, 1, ..., 11, 12)
-///
-/// \remark This is in the inverse of SimpleValueToFaceValueString()
-int SimpleValue(const string& face_value) {}
 
-// TODO: Write a function named SimpleValueToFaceValueString; the function
-// returns a string and takes an int as a parameter
+  return card_v;
+}
 
-/// SimpleValueToFaceValueString takes the \p simple_value of a card and returns
-/// the face_value.
-///
-/// \param simple_value An int representing the value of a card as an int
-/// (0, 1, ..., 11, 12)
-///
-/// \returns A string representing the value of a card (A, 2, ..., Q, K)
-///
-/// \remark This is the inverse of SimpleValue()
-string SimpleValueToFaceValueString(int simple_value) {}
+//   Write a function named IntCardValueToString; the function returns a
+// string and takes an int as a parameter
+string IntCardValueToString(int value) {
+  string card_value;
+  if (value == 1) {
+    card_value = "A";
+  }else if (value == 11){
+    card_value = "J";
+  }else if ( value == 12){
+    card_value = "Q";
+  }else if (value == 13){
+    card_value = "K";
+  }else{
+    card_value = to_string(value);
+  }
+  return card_value;
+}
 
-// TODO: Write a function named SecretCardValue; it returns an int and takes two
-// ints as parameters
 
-/// SecretCardValue returns the face_value of the secret card given the
-/// \p simple_base_value and the \p steps.
-///
-/// Since the \p simple_base_value of the base card is an integer between 0
-/// and 12, we can add \p steps to the \p simple_base_value to find the
-/// simple value of the secret card. To wrap the count around
-/// (from K to A), use modulo 13.
-/// \f$\left(simple\_base\_value + steps \right) \% 13\f$
-///
-/// \param simple_base_value The simple_value of the base card, for
-/// example 3C -> 2
-/// \param steps The number of steps from the base simple_value to the secret
-/// card value
-///
-/// \returns The simple_value of the secret card as an int (0, 1, ..., 11, 12)
-int SecretCardValue(int simple_base_value, int steps) {}
-
-// TODO: Write a function named SuitMultiplier; the function returns an int and
+//   Write a function named SuitMultiplier; the function returns an int and
 // takes a const string& as a parameter.
+int SuitOffset(const string& suit) {
+  string suits = string(1, suit.at(suit.size() - 1));
+  int suit_offset_number;
+  if (suits == "C"){
+    suit_offset_number = 1;
+  }else if (suits == "D"){
+    suit_offset_number = 2;
+  }else if(suits == "H"){
+    suit_offset_number = 3;
+  }else{
+    suit_offset_number = 4;
+  }
+  return suit_offset_number;
+}
 
-/// The offset of each suit; since the cards are ordered value first, suit
-/// second, the function returns the rank of a given \p suit. Suits are
-/// ranked in alphabetical order (C=1, D, H, S).
-///
-/// \param suit A single letter string representing the suit (C, D, H, S)
-///
-/// \returns An int between [1,4] representing the rank of the given suit
-int SuitOffset(const string& suit) {}
-
-// TODO: Write a function named Suit which returns the suit of a given card
-
-/// Given a two to three letter string that represents a \p card, return
-/// a single letter string that represents the card's suit
-///
-/// \param card A two to three letter string representing a card,
-/// ex. AS, 4D, 10H, QS
-///
-/// \returns A single letter string which has the suit of the given card
-string Suit(const string& card) {}
-
-// TODO: Write a function named FaceFalue which returns the face value of
-// a card
-
-/// Given a two to three letter string that represents a \p card, return
-/// a single letter string that represents the card's face_value
-///
-/// \param card A two to three letter string representing a card,
-/// ex. AS, 4D, 10H, QS
-///
-/// \returns A single letter string that which has the face_value of the
-/// card (A, 2, 3, 4, 5, 6, 7, 8, 9, 1, J, Q, K)
-///
-/// \remark Although a 10 has two letters, only the first letter is needed
-/// since it is the only card that has a value that starts with 1.
-string FaceValue(const string& card) {}
-
-// TODO: Write a function named SecretSteps; the function returns an int and
+//   Write a function named SecretSteps; the function returns an int and
 // takes three const string& as parameters.
+int SecretSteps(const string& card1, const string& card2,const string& card3) {
+  int c_one = CardValue(card1) * 4 + SuitOffset(card1);
+  int c_two = CardValue(card2) * 4 + SuitOffset(card2);;
+  int c_three = CardValue(card3) * 4 + SuitOffset(card3);
 
-/// SecretSteps calculates the number of steps that are being signaled given
-/// the order of the three cards.
-///
-/// There are four cards given on the command line. The first card is the base
-/// card. The remaining three cards are \p card_one (argv[2]),
-/// \p card_two (arg[3]), \p card_three (argv[4]).
-///
-/// Given the relative order of these cards, determine the steps to return
-/// (labeled Value to Add in the table).
-///
-/// | Order | Value to Add |
-/// | :--- | :---: |
-/// | low, middle, high | 1 |
-/// | low, high, middle | 2 |
-/// | middle, low, high | 3 |
-/// | middle, high low | 4 |
-/// | high, low, middle | 5 |
-/// | high, middle, low | 6 |
-///
-/// \param card_one The first card, expressed as a 2-3 letter string
-/// \param card_two The second card, expressed as a 2-3 letter string
-/// \param card_three The third card, expressed as a 2-3 letter string
-///
-/// \returns The number of steps to add (labeled Value Add in the table)
-int SecretSteps(const string& card_one, const string& card_two,
-                const string& card_three) {}
+  array<int, 3> c_list = {c_one, c_two, c_three};
 
-/// Entry point to the mind_reader program
-/// \remark Must have at least 5 command line arguments. Each argument
-/// represents a card.
+  int low = 0;
+  int high = 0;
+  int middle = 0;
+
+  for (int i = 0; i < 3; i++){
+    for (int j = 3; j > 2; j--){
+      try{
+        if ((c_list.at(i) <= c_list.at(j-1)) & (c_list.at(i) <= c_list.at(j-2)) & (c_list.at(i) <=c_list.at(j-3))){
+          low = c_list.at(i);
+
+        }else if((c_list.at(i) >= c_list.at(j-1)) & (c_list.at(i) >= c_list.at(j-2)) & (c_list.at(i) >= c_list.at(j-3))  ){
+          high = c_list.at(i);
+
+        }else{
+          middle = c_list.at(i);
+
+        }
+      }catch(const exception& e){
+        cout << "Oops, there was an error. \n";
+
+      }
+    }
+  }
+
+
+  int steps = 0;
+  array<int, 3> one = {low, middle, high};
+  array<int, 3> two = {low, high, middle};
+  array<int, 3> three = {middle, low, high};
+  array<int, 3> four = {middle, high, low};
+  array<int, 3> five = {high, low, middle};
+  array<int, 3> six = {high, middle, low};
+
+
+  if(c_list == one){
+    steps = 1;
+  }else if(c_list == two){
+    steps = 2;
+  }else if (c_list == three){
+    steps = 3;
+  }else if (c_list == four){
+    steps = 4;
+  }else if (c_list == five){
+    steps = 5;
+  }else if (c_list == six){
+    steps = 6;
+  }else{
+    cout << "Oh no, something went wrong.\n";
+  }
+
+  return steps;
+
+}
+
+
+
+
+
+//   Write a function named SecretCardValue; it returns an int and takes two
+// ints as parameters
+int SecretCardValue(string base_value, string one, string two, string three) {
+  int base_card = CardValue(base_value);
+  int secret_card = ((base_card + SecretSteps(one, two, three)) % 13);
+  string secret_string = to_string(secret_card);
+  string base_string = to_string(base_card);
+  string var1 = IntCardValueToString(CardValue(secret_string));
+  string var2 = string(1, base_value.at(base_value.size() - 1));
+
+
+  cout << "Your card is " << var1 << var2 << ".\n";
+  return 0;
+}
+
+
+
 int main(int argc, char const* argv[]) {
   if (argc < 5) {
     ErrorMessage();
     return 1;
   }
   string base_card = string(argv[1]);
-  string card_one = string(argv[2]);
-  string card_two = string(argv[3]);
-  string card_three = string(argv[4]);
+  string card1 = string(argv[2]);
+  string card2 = string(argv[3]);
+  string card3 = string(argv[4]);
 
+  CardValue(base_card);
+  CardValue(card1);
+  CardValue(card2);
+  CardValue(card3);
+
+
+  SuitOffset(base_card);
+  SuitOffset(card1);
+  SuitOffset(card2);
+  SuitOffset(card3);
+
+  SecretSteps(card1, card2, card3);
+
+
+  SecretCardValue(base_card, card1, card2, card3);
+
+  //CardValue(Value(base_card))
+
+// Start with the base_card
+// - get the base_card's value as a string Value(base_card)
+// - convert the base_card's string value to an int CardValue()
+// - get the base card's suit and save it to a string
+// Get the number if steps to add to the base card value SecretSteps()
   return 0;
 }
